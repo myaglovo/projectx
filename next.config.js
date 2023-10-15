f07f6dd3268b76745e21bb15235bfef5a3a314fc
Антон Кошкин <antonkoskin@MacBook-Air-Anton.local>
2023-10-15 18:42:42 +0300

    sdfsdf

--- next.config.js
+++ next.config.js
@@ -16,9 +16,9 @@ module.exports = {
     PASSWORD: "oE8iU2uN4rqX6rB2",
     DATABASE: "u2015014_projectx",
     NEXTAUTH_SECRET: "Ey7nTKnggBc0bRN8WUjyShw2qzOZ6KW4fUyqcKBePxY=",
-    NEXTAUTH_URL: "https://projectx-orpin.vercel.app/",
+    NEXTAUTH_URL: "https://projectx-orpin.vercel.app",
     // NEXTAUTH_URL: "http://localhost:3000",
-    NEXT_PUBLIC_URL: "https://projectx-orpin.vercel.app/",
+    NEXT_PUBLIC_URL: "https://projectx-orpin.vercel.app",
     // NEXT_PUBLIC_URL: "http://localhost:3000",
   },
 };
