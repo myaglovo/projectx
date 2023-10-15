f50745a8fb9451f76297508285c3aa2c17219d41
Антон Кошкин <antonkoskin@MacBook-Air-Anton.local>
2023-10-15 19:00:24 +0300

    sdf

--- app/events/page.js
+++ app/events/page.js
@@ -28,7 +28,7 @@ export default function Calendar() {
       cache: "no-store",
     };
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_URL}/api/events`,
+      `${process.env.NEXT_PUBLIC_URL}/api/events/date`,
       getData
     );
     const events = await res.json();
